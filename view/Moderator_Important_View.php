62313ddbde62951862d6b7146ac5e584756c33ad
NIPUNMADHUSANKA <2019cs084@stu.ucsc.lk>
2021-09-26 02:39:26 +0530

    view_important_number

---
+++ view/Moderator_Important_View.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moderator_Home</title>
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/moderator.css">
+</head>
+
+<style>
+  body {
+  overflow: hidden; /* Hide scrollbars */
+  }
+</style>
+
+<body>
+
+  <div class="heder">
+      
+        <div class="left">
+            <img src="../images/logo.svg" alt="" srcset="">
+        </div>
+
+        <div class="center">
+            <img src="../images/Name.svg" alt="" srcset="">
+        </div>
+
+        <div class="right">
+            <img src="../images/Profile.svg" alt="" srcset="">
+            <p>A.A.N.Madhusanka <img src="../images/Drop-down.svg" alt="" srcset="" class="down"> </p>
+            <ul class="profile_menu">
+                <li><a href="#"> <img src="../images/other/profile.svg" alt="" srcset=""> My Profile</a></li>
+                <li><a href="#"><img src="../images/other/location.svg" alt="" srcset="">Select Area</a></li>
+                <li><a href="#"><img src="../images/other/type.svg" alt="" srcset="">Select Type</a></li>
+                <li><a href="#"><img src="../images/other/insights.svg" alt="" srcset="">Insights</a></li>
+                <li><a href="#"><img src="../images/other/deactivate.svg" alt="" srcset="">Deactivate</a></li>
+                <li><a href="#"><img src="../images/other/logout.svg" alt="" srcset="">Log Out</a></li>
+            </ul>
+        </div>
+
+  </div>
+  
+  <ul class="menu">
+      <li><a href="#">Home</a></li>
+      <li class="view dropdown">
+        <a href="javascript:void(0)" class="dropbtn">View</a>
+        <div class="view-content dropdown-content">
+            <a href="#">News</a>
+            <a href="#">Article</a>
+            <a href="#">Notice</a>
+            <a href="#">job vacancy</a>
+            <a href="#">Commercial Ad</a>
+        </div>
+      </li>
+
+      <li class="publish dropdown">
+        <a href="javascript:void(0)" class="dropbtn">Publish</a>
+        <div class="publish-content dropdown-content">
+            <a href="#">Pending</a>
+            <a href="#">Set Time</a>
+        </div>
+      </li>
+
+      <li class="imporatnt dropdown">
+        <a href="javascript:void(0)" class="dropbtn">Important Contact</a>
+        <div class="important-content dropdown-content">
+            <a href="#">View Contact Number</a>
+            <a href="#">Edit Contact Number</a>
+        </div>
+      </li>
+
+      <li><a href="#">Insights</a></li>
+      <li class="more dropdown">
+        <a href="javascript:void(0)" class="dropbtn">More</a>
+        <div class="more-content dropdown-content">
+            <a href="#">Save</a>
+            <a href="#">Hidden</a>
+            <a href="#">Reminder</a>
+        </div>
+      </li>
+  </ul>
+
+<!-- Moderator Important Number View -->
+<div class="content_important_view">
+    <div class="important_contact_view_head">
+      <div class="imv_head">
+        Important Contact Number
+      </div>
+
+      <div class="imv_sort">
+        <div class="imv_sort_bar">
+          <button onclick="" class="drop_area_sort">Select Area</button>
+          <div class="drop_area_sort_cont ent">
+            <input type="text" id="myInput" onkeyup="" placeholder="Search...">
+            <a href="#">Gampaha</a>
+            <a href="#">Minuwangoda</a>
+            <a href="#">Negombo</a>
+          </div>
+        </div>
+      </div>
+        
+    </div>
+      
+</div>
+    
+</body>
+</html>
