799012c40b6690ceb3350549d16068f049dcf1bd
NIPUNMADHUSANKA <2019cs084@stu.ucsc.lk>
2021-10-05 01:18:53 +0530

    Moderator Smart Calendar

--- view/Moderator_Smart_Calendar.php
+++ view/Moderator_Smart_Calendar.php
@@ -15,7 +15,9 @@
   body {
     overflow-x: hidden; /* Hide scrollbars */
   }
-  
+  .box_head:hover img{
+    opacity: 1;
+  } 
   .post_sort{
       padding-left:80px;
   }
@@ -45,7 +47,93 @@
     cursor: pointer;
   }
 
+  .box-container:hover{
+    transform: scale(1.6);
+  }
 
+  .box-container{
+    transform: scale(1.6);
+    margin-top: 6rem;
+    margin-left: 1.8rem;
+  }
+
+  .box_body h3{
+      font-size:0.9rem;
+  }
+
+  .box_body p{
+      font-size:0.65rem;
+  }
+
+  .box-read{
+    width:800px;
+    height:400px;
+    margin-top:0.2rem;
+    padding:1rem;
+    overflow: hidden;
+    overflow-y:scroll;
+  }
+
+  .box-read h2{
+    font-size:1.8rem;
+    font-weight:normal;
+    color:#222;
+  }
+
+  .box-read p{
+    font-weight:normal;
+    font-size:1rem;
+    padding:1rem;
+    text-align:justify;
+    color:#555;
+    letter-spacing:1.8px;
+    line-height:30px;
+  }
+
+  .button-set{
+    margin-top:-12rem;
+    margin-left:3rem;
+    display:flex;
+    flex-direction:row;
+  }
+
+  .view_btn{
+    width:100px;
+    margin-top:20%;
+    margin-left:40rem;
+    box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.4);
+  }
+
+  .update_btn{
+    background-color: #ADD8E6;
+    color:#222;
+  }
+
+  .remove_btn{
+   margin-top:15.2rem;
+   background-color:#FF4444;
+   margin-left:3rem;
+   color:#222;
+  }
+
+  .close_btn{
+    position: absolute;
+    margin-left:90%;
+    margin-top:1%;
+  }
+
+
+  .popup .content{
+    width:330px;
+  }
+
+  .inp1{
+    margin-left:-0.5rem;
+  }
+
+  .update_btn{
+    text-align:center;
+  }
 
 
 </style>
@@ -119,12 +207,56 @@
 
 <!-- Moderator Notices View -->
 
+<div class="close_btn">
+    <img src="../images/close_btn.svg" alt="" srcset="">
+</div>
+
+
+<div class="posts_content_view_body">
+
+    <div class="body_information">
+         
+          <div class="box-container">
+              <div class="box_head">
+                <img src="../images/sethma.jpeg" alt="">
+              </div>
+              <div class="box_body">
+                <h3>Sethma Hospital</h3>
+                <p>Publish Date</p>
+                <p>Create By</p>
+              </div>
+          </div>
+
+
+
+          <div class="box-read">
+             <h2>Lorem Ipsum</h2>
+             <p>
+             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+             Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+
+            
+             </p>
+          </div>
+    </div>
+
+    <div class="button-set">
+        <div class="view_btn update_btn" onclick="togglePopup()">Update</div>
+        <div class="view_btn remove_btn">Remove</div>
+    </div>
+</div>
+
+
+
+
 
 
 <!--create popup window-->
 
 
-<div class="popup" id="popup-1">
+<div class="popup" id="popup-3">
 
       <div class="overlay"></div>
 
@@ -139,18 +271,18 @@
               <hr>
 
               <div class="popup_form">
-                  <h3 class="popup_title">Update Publish Date & Time</h3>
+                  <h3 class="popup_title">Update Smart Calendar</h3>
                   <form action="" method="post">
 
-                     <label for="update-date" class="lbl">Date</label>
+                     <label for="update-date" class="lbl">Old Date</label>
                   
                      <input type="date" name="" id="update-date" class="inp">
                      <br>
                      <br>
 
-                     <label for="update-time" class="lbl">Time</label>
-                     
-                     <input type="time" name="" id="update-time" class="inp">
+                     <label for="new-date" class="lbl">New Date</label>
+                  
+                     <input type="date" name="" id="new-date" class="inp inp1">
                      <br>
 
                      <div class="update_btn">Update</div>
@@ -170,30 +302,14 @@
 
 
 <script>
-    function showsort() {
-      document.getElementById("sortdrop").classList.toggle("show");
-    }
+
 
     function togglePopup(){
-      document.getElementById("popup-1").classList.toggle("active");
+      document.getElementById("popup-3").classList.toggle("active");
     }
 
 
-    function filterFunction() {
-      var input, filter, ul, li, a, i;
-      input = document.getElementById("myInput");
-      filter = input.value.toUpperCase();
-      div = document.getElementById("sortdrop");
-      a = div.getElementsByTagName("a");
-      for (i = 0; i < a.length; i++) {
-            txtValue = a[i].textContent || a[i].innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                 a[i].style.display = "";
-            } else {
-                 a[i].style.display = "none";
-        }
-      }
-    }
+   
 
 </script>
     
