225218fd9f1cc2f09d6e95dd0d44224692a8f157
NIPUNMADHUSANKA <2019cs084@stu.ucsc.lk>
2021-10-08 21:31:05 +0530

    main page

---
+++ view/Main_Home.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moderator_Home</title>
+   <!-- <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/moderator.css">-->
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+
+<style>
+  body {
+   overflow-x:hidden;/* Hide scrollbars */
+  }
+
+</style>
+
+<body>
+
+  <div class="heder">
+      
+        <div class="left">
+            <img src="../images/Name.svg" alt="" srcset="">
+        </div>
+
+        
+
+        <div class="right">
+
+                <ul>
+                    <li> <a href="#">Home</a> </li>
+                    <li> <a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            
+        </div>
+
+  </div>
+  
+ 
+<!-- Moderator Page Content -->
+<div class="content">
+
+        <div class="content_left">
+          <h1>NEWSLIA</h1>
+              
+              <div class="content_left_info">
+
+                NEWSLIA, is the best news reporting system <br> which provides
+                news and special <br> announcements that are related to <br> the user's area.
+              </div>
+
+              <div class="button_set">
+                  <div class="button_login">Login</div>
+                  <div class="button_login">Sign Up</div>
+              </div>
+
+        </div>
+
+        <div class="content_right">
+            <img src="../images/background.svg" alt="" srcset="">
+        </div>
+
+  </div>
+    
+</body>
+</html>
