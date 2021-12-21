50844deb96f97c907b5a392d79d780523ba43ce3
NIPUNMADHUSANKA <2019cs084@stu.ucsc.lk>
2021-12-21 20:08:44 +0530

    Post Type

--- view/Moderate_Post_Type.php
+++ view/Moderate_Post_Type.php
@@ -21,7 +21,7 @@
 
 <style>
   body {
-    overflow: hidden; /* Hide scrollbars */
+    /*overflow: hidden; /* Hide scrollbars */
   }
 
   
@@ -91,7 +91,7 @@
         width: 30%;
         padding: 20px;
         margin: auto;
-        height: 250px;
+        height: 220px;
         overflow-y: scroll;
     }
 
@@ -139,7 +139,8 @@
 <!--navigation-->
 
 <?php $page = 'home';
-  include 'nav.php'; ?>
+  include 'nav.php'; 
+?>
 
 <!--End of Navigation-Bar-->
 
@@ -197,87 +198,213 @@
 
 </div>";
 
+  $post_type_sql = "SELECT * FROM post_type WHERE System_Actor_Id = '$Moderator_ID'";
+  $post_type_statement = $conn->query($post_type_sql);
+  $post_type_results = $post_type_statement ->fetchAll(PDO::FETCH_ASSOC);
+  
+  if($post_type_results){
+    foreach($post_type_results as $post_type_result){
+        echo "<hr style='margin-top: 45px;margin-left:2rem;'>
+        </div>
+            <div class='topic'>
+                <h3>Select Posts Type</h3>
+            </div>
+        
+        
+            <div class='arealist'>
+                
+            <form action='Moderate_Post_Type.php' method='post'>
+                
+                <input type='checkbox'";
+                  if($post_type_result['News']==1){echo 'checked';}
+                echo " class = 'post_type_chk' disabled name='News'>
+                <label>
+                  <h4>News</h4>
+                </label>";
+
+              
+              $news_type_sql = "SELECT * FROM news_type WHERE System_Actor_Id = '$Moderator_ID'";
+              $news_type_statement = $conn ->query($news_type_sql);
+              $news_type_results = $news_type_statement ->fetchAll(PDO::FETCH_ASSOC);
+              
+              if($news_type_results){
+                foreach($news_type_results as $news_type_result){
+                      echo "<ol>
+                      <li><input type='checkbox'";if($news_type_result['Political'] == 1){echo "checked";}  echo " class = 'post_type_chk' disabled name='Political'><label>Political</label></li>
+                      <li><input type='checkbox'";if($news_type_result['Crime'] == 1){echo "checked";}  echo " class = 'post_type_chk' disabled name='Crime'><label>Crime</label></li>
+                      <li><input type='checkbox'";if($news_type_result['Inves'] == 1){echo "checked";}  echo " class = 'post_type_chk' disabled name='Inves'><label>Investigative</label></li>
+                      <li><input type='checkbox'";if($news_type_result['Art'] == 1){echo "checked";}  echo " class = 'post_type_chk' disabled name='Art'><label>Arts</label></li>
+                      <li><input type='checkbox'";if($news_type_result['Eduation'] == 1){echo "checked";}  echo " class = 'post_type_chk' disabled name='Eduation'><label>Entertainment</label></li>
+                      <li><input type='checkbox'";if($news_type_result['Sport'] == 1){echo "checked";}  echo " class = 'post_type_chk' disabled name='Sport'><label>Education</label></li>
+                      <li><input type='checkbox'";if($news_type_result['Entertainment'] == 1){echo "checked";}  echo " class = 'post_type_chk' disabled name='Entertainment'><label>Sports</label></li>
+                      <li><input type='checkbox'";if($news_type_result['Environment'] == 1){echo "checked";}  echo " class = 'post_type_chk' disabled name='Environment'><label>Environment</label></li>
+                      <li><input type='checkbox'";if($news_type_result['Other'] == 1){echo "checked";}  echo " class = 'post_type_chk' disabled name='Other'><label>Other</label></li>
+                    </ol>";
+                }
+              }
+            
+              echo  "<br>
+                        <input type='checkbox'";
+                          if($post_type_result['Article']==1){echo 'checked';}
+                        echo " class = 'post_type_chk' disabled name='Article'>
+                        <label>
+                          <h4>Articles</h4>
+                        </label>
+                                    
+                    <br>
+                       <input type='checkbox'";
+                        if($post_type_result['Notice']==1){echo 'checked';}
+                       echo " class = 'post_type_chk' disabled name='Notice'>
+                       <label>
+                          <h4>Notices</h4>
+                       </label>
+                                    
+                    <br>
+                      <input type='checkbox'";
+                       if($post_type_result['Job_Vacancies']==1){echo 'checked';}
+                      echo " class = 'post_type_chk' disabled name='Job_Vacancies'>
+                      <label>
+                        <h4>Job Vacancies</h4>
+                      </label>
+        
+                    <br>
+                      <input type='checkbox'";
+                        if($post_type_result['Com_Ads']==1){echo 'checked';}
+                      echo " class = 'post_type_chk' disabled name='Com_Ads'>
+                      <label>
+                        <h4>Commercial Advertisements</h4>
+                      </label>
+        
+          </div>
+        </div>
+        <div class='btn_set'>
+           
+           <input type='button' value='Edit' class='edit_btn_set' onclick='remove_disable()'>
+           <br>
+           <input type='submit' value='Save' class='save_btn_set' name = 'Save_POST'>
+
+           </form>
+        </div>
+</div>";
+      
+    }
+  }
+
+
+  if(isset($_POST['Save_POST'])){
+
+    $News = (isset($_POST['News']) == 1) ? 1 : 0;
+    $Political = (isset($_POST['Political']) == 1) ? 1 : 0;
+    $Inves = (isset($_POST['Inves']) == 1) ? 1 : 0;
+    $Art = (isset($_POST['Art']) == 1) ? 1 : 0;
+    $Eduation = (isset($_POST['Eduation']) == 1) ? 1 : 0;
+    $Sport = (isset($_POST['Sport']) == 1) ? 1 : 0;
+    $Entertainment = (isset($_POST['Entertainment']) == 1) ? 1 : 0;
+    $Environment = (isset($_POST['Environment']) == 1) ? 1 : 0;
+    $Crime = (isset($_POST['Crime']) == 1) ? 1 : 0;
+    $Other = (isset($_POST['Other']) == 1) ? 1 : 0;
+
+    $Article = (isset($_POST['Article']) == 1) ? 1 : 0;
+    $Notice = (isset($_POST['Notice']) == 1) ? 1 : 0;
+    $Job_Vacancies = (isset($_POST['Job_Vacancies']) == 1) ? 1 : 0;
+    $Com_Ads = (isset($_POST['Com_Ads']) == 1) ? 1 : 0;
+
+    
+    if($News == 1){
+      $Political = 1;
+      $Inves = 1;
+      $Art = 1;
+      $Eduation = 1;
+      $Sport = 1;
+      $Entertainment = 1;
+      $Environment = 1;
+      $Crime = 1;
+      $Other = 1;
+
+    }
+
+    $POST = [
+      'Moderator_ID' => $Moderator_ID,
+      'News' => $News,
+      'Article' => $Article,
+      'Notice' => $Notice,
+      'Job_Vacancies' => $Job_Vacancies,
+      'Com_Ads' => $Com_Ads
+    ];
+    
+    $sql_1 = 'UPDATE post_type
+            SET News = :News, Article = :Article, Notice = :Notice, Job_Vacancies = :Job_Vacancies, Com_Ads = :Com_Ads 
+            WHERE System_Actor_Id = :Moderator_ID';
+    
+    // prepare statement
+    $statement_1 = $conn->prepare($sql_1);
+    
+    // bind params
+    $statement_1->bindParam(':Moderator_ID', $POST['Moderator_ID']);
+    $statement_1->bindParam(':News', $POST['News'],PDO::PARAM_INT);
+    $statement_1->bindParam(':Article', $POST['Article'],PDO::PARAM_INT);
+    $statement_1->bindParam(':Notice', $POST['Notice'],PDO::PARAM_INT);
+    $statement_1->bindParam(':Job_Vacancies', $POST['Job_Vacancies'],PDO::PARAM_INT);
+    $statement_1->bindParam(':Com_Ads', $POST['Com_Ads'],PDO::PARAM_INT);
+    
+    // execute the UPDATE statment
+    $statement_1->execute();
+       
+    
+
+    $POST = [
+      'Moderator_ID' => $Moderator_ID,
+      'Political' => $Political,
+      'Crime' => $Crime,
+      'Inves' => $Inves,
+      'Art' => $Art,
+      'Eduation' => $Eduation,
+      'Sport' => $Sport,
+      'Entertainment' => $Entertainment,
+      'Environment' => $Environment,
+      'Other' => $Other
+    ];
+    
+    $sql_2 = 'UPDATE news_type
+            SET Political = :Political, Crime = :Crime, Inves = :Inves, Art = :Art, Eduation = :Eduation, 
+            Sport = :Sport, Entertainment = :Entertainment, Environment = :Environment, Other = :Other 
+            WHERE System_Actor_Id = :Moderator_ID';
+    
+    // prepare statement
+    $statement_2 = $conn->prepare($sql_2);
+    
+    // bind params
+    $statement_2->bindParam(':Moderator_ID', $POST['Moderator_ID']);
+    $statement_2->bindParam(':Political', $POST['Political'],PDO::PARAM_INT);
+    $statement_2->bindParam(':Crime', $POST['Crime'],PDO::PARAM_INT);
+    $statement_2->bindParam(':Inves', $POST['Inves'],PDO::PARAM_INT);
+    $statement_2->bindParam(':Art', $POST['Art'],PDO::PARAM_INT);
+    $statement_2->bindParam(':Eduation', $POST['Eduation'],PDO::PARAM_INT);
+    $statement_2->bindParam(':Sport', $POST['Sport'],PDO::PARAM_INT);
+    $statement_2->bindParam(':Entertainment', $POST['Entertainment'],PDO::PARAM_INT);
+    $statement_2->bindParam(':Environment', $POST['Environment'],PDO::PARAM_INT);
+    $statement_2->bindParam(':Other', $POST['Other'],PDO::PARAM_INT);
+    
+    // execute the UPDATE statment
+    if($statement_2->execute()){
+        echo "<script>window.open('./Moderate_Post_Type.php','_self'); </script>";
+    }
+
+
+
+  }
+
+
 ?>
-
-  <hr style="margin-top: 45px;margin-left:2rem;">
-</div>
-
-
-            <div class="topic">
-                            <h3>Select Posts Type</h3>
-                        </div>
-
-
-                        <div class="arealist">
-                            <input type="checkbox"><label>
-                                <h4>News</h4>
-                            </label>
-                            <ol>
-                                <li><input type="checkbox"><label>Political</label></li>
-                                <li><input type="checkbox"><label>Crime</label></li>
-                                <li><input type="checkbox"><label>Investigative</label></li>
-                                <li><input type="checkbox"><label>Arts</label></li>
-                                <li><input type="checkbox"><label>Entertainment</label></li>
-                                <li><input type="checkbox"><label>Education</label></li>
-                                <li><input type="checkbox"><label>Sports</label></li>
-                                <li><input type="checkbox"><label>Environment</label></li>
-                                <li><input type="checkbox"><label>Other</label></li>
-                            </ol>
-
-                            <br>
-                            <input type="checkbox"><label>
-                                <h4>Articles</h4>
-                            </label>
-                            
-                            <br>
-
-                            <input type="checkbox"><label>
-                                <h4>Notices</h4>
-                            </label>
-                            <br>
-                            <input type="checkbox"><label>
-                                <h4>Job Vacancies</h4>
-                            </label>
-
-                            <br>
-                            <input type="checkbox"><label>
-                                <h4>Commercial Advertisements</h4>
-                            </label>
-
-                        </div>
-
-                        
-
-
-
-            </div>
-            <div class="btn_set">
-                <button class="edit_btn_set" onclick="remove_disable()">Edit</button>
-                <br>
-                <button class="save_btn_set">Save</button>
-            </div>
-
-</div>
-
 
 
 <script>
     function remove_disable(){
-      var input = document.getElementsByClassName('moderator_radio');
+      var input = document.getElementsByClassName('post_type_chk');
       for (var i = 0; i < input.length; i++) {
                 input[i].disabled = false;
             }
     }
-
-    function remove_read_disable(){
-      var input = document.getElementsByClassName('moderator_read_radio');
-      for (var i = 0; i < input.length; i++) {
-                input[i].disabled = false;
-                
-            }
-    }
-
-
 </script>
    
 </body>
